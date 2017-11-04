639612a643f6a5feadad8e3a395a54a3cbf6d2de
Ramiz Raja <informramiz@gmail.com>
2017-11-05 04:31:39 +0500

    feat: Use val in string text

--- MyAndroidKotlinApplication/app/src/main/java/com/materialdesign/ramiz/myandroidkotlin/MainActivity.kt
+++ MyAndroidKotlinApplication/app/src/main/java/com/materialdesign/ramiz/myandroidkotlin/MainActivity.kt
@@ -7,6 +7,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity: AppCompatActivity() {
 
@@ -18,6 +19,9 @@ class MainActivity: AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Fab clicked", Snackbar.LENGTH_SHORT);
         }
+
+        val x: Int = 0
+        textView.text = "Value of x is $x"
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
