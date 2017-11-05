a790ec06e43c1d711ed0001322559417982a3504
Ramiz Raja <informramiz@gmail.com>
2017-11-05 15:16:59 +0500

    feat: Try subclassing a View class

---
+++ MyAndroidKotlinApplication/app/src/main/java/com/materialdesign/ramiz/myandroidkotlin/MyCustomView.kt
@@ -0,0 +1,14 @@
+package com.materialdesign.ramiz.myandroidkotlin
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+/**
+ * Created by ramiz on 11/5/17.
+ */
+
+open class MyCustomView : View {
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+}
