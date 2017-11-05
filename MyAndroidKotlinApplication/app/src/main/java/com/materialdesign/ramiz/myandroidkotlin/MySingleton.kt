4005290388dc6d901b43daf6c0b963103e6ff187
Ramiz Raja <informramiz@gmail.com>
2017-11-05 15:58:50 +0500

    feat: Try singleton, the Kotlin way

---
+++ MyAndroidKotlinApplication/app/src/main/java/com/materialdesign/ramiz/myandroidkotlin/MySingleton.kt
@@ -0,0 +1,10 @@
+package com.materialdesign.ramiz.myandroidkotlin
+
+/**
+ * Created by ramiz on 11/5/17.
+ */
+object MySingleton {
+    fun doSomething() {
+        print("Right now I am sleeping and I am lazy")
+    }
+}
