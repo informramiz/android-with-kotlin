21e7371170b1db45cca56ce13eecca5cf2e2d459
Ramiz Raja <informramiz@gmail.com>
2017-11-05 16:24:56 +0500

    feat: Try class property with custom accessors

--- MyAndroidKotlinApplication/app/src/main/java/com/materialdesign/ramiz/myandroidkotlin/Customer.kt
+++ MyAndroidKotlinApplication/app/src/main/java/com/materialdesign/ramiz/myandroidkotlin/Customer.kt
@@ -4,6 +4,14 @@ package com.materialdesign.ramiz.myandroidkotlin
  * Created by ramiz on 11/5/17.
  */
 class Customer(val id: String, override val name: String, override val age: Int) : Person(name, age) {
+    //let's declare some property
+    var myProperty: String = name.toUpperCase()
+        get() = this.toString()
+        set(value) {
+            if (value.isNotEmpty()) field = value
+        }
+
+
     //secondary constructors
     constructor(customer: Customer) : this(customer.id, customer.name, customer.age) {
 
