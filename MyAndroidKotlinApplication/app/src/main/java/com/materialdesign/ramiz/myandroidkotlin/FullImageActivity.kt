479753a4e283009070b6eaf01a2ab1f2304ff238
Ramiz Raja <informramiz@gmail.com>
2017-11-06 05:45:56 +0500

    feat: Enter immersive mode on singleTapUp

--- MyAndroidKotlinApplication/app/src/main/java/com/materialdesign/ramiz/myandroidkotlin/FullImageActivity.kt
+++ MyAndroidKotlinApplication/app/src/main/java/com/materialdesign/ramiz/myandroidkotlin/FullImageActivity.kt
@@ -3,17 +3,24 @@ package com.materialdesign.ramiz.myandroidkotlin
 import android.annotation.TargetApi
 import android.os.Build
 import android.os.Bundle
+import android.support.v4.view.GestureDetectorCompat
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.View
+import android.view.Window
 import com.materialdesign.ramiz.myandroidkotlin.Imagedownloader.ImageDownloaderSingleton
 
 import kotlinx.android.synthetic.main.activity_full_image.*
 import kotlinx.android.synthetic.main.content_full_image.*
 
-class FullImageActivity : AppCompatActivity() {
+class FullImageActivity : AppCompatActivity(){
     companion object {
         val EXTRA_URL = "image_url"
     }
+
+    private var mSimpleGestureDetector: GestureDetector? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +30,19 @@ class FullImageActivity : AppCompatActivity() {
         val imageUrl = intent.getStringExtra(EXTRA_URL)
         ImageDownloaderSingleton.loadImage(this, imageUrl, fullImageView)
 
+        mSimpleGestureDetector = GestureDetector(this, object : GestureDetector.SimpleOnGestureListener(){
+            override fun onSingleTapUp(e: MotionEvent?): Boolean {
+                toggleImmersiveMode()
+                return true
+            }
+        })
+
         registerListeners()
         enterImmersiveMode()
     }
 
     private fun registerListeners() {
-        this.window.decorView.setOnSystemUiVisibilityChangeListener({
+        this.window.decorView.setOnSystemUiVisibilityChangeListener { it ->
             //system bars will only be "visible" if none of the
             // LOW_PROFILE, HIDE_NAVIGATION, or FULLSCREEN flags are set.
             if ((it and View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
@@ -39,12 +53,30 @@ class FullImageActivity : AppCompatActivity() {
             } else {
                 supportActionBar?.hide()
             }
-        })
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        mSimpleGestureDetector?.onTouchEvent(event)
+        return super.onTouchEvent(event)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         exitImmersiveMode()
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private fun isSystemUiVisible(): Boolean {
+        return window.decorView.systemUiVisibility and View.SYSTEM_UI_FLAG_IMMERSIVE != View.SYSTEM_UI_FLAG_IMMERSIVE
+    }
+
+    private fun toggleImmersiveMode() {
+        when(isSystemUiVisible()) {
+            true -> enterImmersiveMode()
+            else -> exitImmersiveMode()
+        }
+
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
@@ -55,11 +87,14 @@ class FullImageActivity : AppCompatActivity() {
                 or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                 or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                 or View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                or View.SYSTEM_UI_FLAG_IMMERSIVE
                 )
     }
 
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private fun exitImmersiveMode() {
-        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+        window.decorView.systemUiVisibility = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
     }
 }
